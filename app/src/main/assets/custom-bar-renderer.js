ea968aa81698485aff18ecfc227d7276854d4f24
grandiloquent <1785907865@qq.com>
2022-10-15 15:18:24 +0800

    更新

--- app/src/main/assets/custom-bar-renderer.js
+++ app/src/main/assets/custom-bar-renderer.js
@@ -43,7 +43,7 @@ class CustomBarRenderer extends HTMLElement {
 }</style>
     <div style="display: flex; justify-content: space-around; position: fixed; bottom: 0; left: 0; right: 0; padding: 0 env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left); z-index: 3; height: 48px; border-top: 1px solid rgba(0,0,0,.1); background: rgba(255,255,255,.98); color: #030303; font-size: 1.1rem;">
       <div style="display: flex; -webkit-box-flex: 1; flex: 1 1 0%; min-width: 0;">
-        <div class="item">
+        <div class="item" id="home">
           <div class="image">
             <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24" viewBox="0 0 24 24" width="24">
               <g>
@@ -74,7 +74,15 @@ class CustomBarRenderer extends HTMLElement {
       const customFavorites = document.createElement('custom-favorites');
       document.body.appendChild(customFavorites);
     })
-    
+    const home = this.root.querySelector('#home');
+    home.addEventListener('click', evt => {
+      window.location = "/"
+    })
+
+
+
+
+
 
 
   }
