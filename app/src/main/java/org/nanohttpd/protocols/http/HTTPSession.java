931e23556f53be3e29decbd36e5ed0d8d194030d
grandiloquent <1785907865@qq.com>
2022-10-24 05:53:32 +0800

    更新

--- app/src/main/java/org/nanohttpd/protocols/http/HTTPSession.java
+++ app/src/main/java/org/nanohttpd/protocols/http/HTTPSession.java
@@ -33,6 +33,7 @@ package org.nanohttpd.protocols.http;
  * #L%
  */
 
+import android.net.Uri;
 import android.util.Log;
 
 import java.io.BufferedInputStream;
@@ -630,13 +631,14 @@ public class HTTPSession implements IHTTPSession {
      * The full path to the saved file is returned.
      */
     private String saveTmpFile(ByteBuffer b, int offset, int len, String filename_hint) {
+
         String path = "";
         if (len > 0) {
             FileOutputStream fileOutputStream = null;
             try {
                 //ITempFile tempFile = this.tempFileManager.createTempFile(filename_hint);
                 ByteBuffer src = b.duplicate();
-                fileOutputStream = new FileOutputStream(filename_hint);
+                fileOutputStream = new FileOutputStream(Uri.decode(filename_hint));
                 FileChannel dest = fileOutputStream.getChannel();
                 src.position(offset).limit(offset + len);
                 dest.write(src.slice());
